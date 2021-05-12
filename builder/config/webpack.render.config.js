08cb08b7f9c8f446ae6feeb6333aed1e49252314
kouyidong <1223519172@qq.com>
2021-05-12 10:54:25 +0800

    锁定依赖版本、增加 html-webpack-plugin、增加打包前删除 output 目录

--- builder/config/webpack.render.config.js
+++ builder/config/webpack.render.config.js
@@ -1,5 +1,6 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 const baseConfig = require('./webpack.base.config')
 
 const renderConfig = {
@@ -15,10 +16,18 @@ const renderConfig = {
   },
   target: 'electron-renderer',
   plugins: [
+    /**
+     * 如果不传入配置则默认在 output.path 目录下生成一个 index.html 文件，并且在 index.html 文件中插入 output.filename 中的 js 文件。
+     * https://github.com/jantimon/html-webpack-plugin#configuration
+     * https://juejin.cn/post/6844903853708541959
+     */
     new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      mode: baseConfig.mode
+      title: 'electron-vue3-webpack-ts', // 网页 document.title 的配置, 在index.html 文件中可以使用 <%= htmlWebpackPlugin.options.title %> 设置网页标题为这里设置的值。
+      filename: 'index.html', // html 文件生成的名称， 默认为 index.html
+      template: 'index.html', // 生成 filename 文件的模版， 如果存在 src/index.ejs， 那么默认将会使用这个文件作为模版。
+      hash: true, // 如果为真，则向所有包含的 js 和 CSS 文件附加一个惟一的 webpack 编译散列。这对于更新每次的缓存文件名称非常有用
+      cache: true, // 设置 js css 文件的缓存，当文件没有发生变化时， 是否设置使用缓存
+      showErrors: true, // 当文件发生错误时， 是否将错误显示在页面
     })
   ]
 }
--- builder/scripts/dev.js
+++ builder/scripts/dev.js
@@ -1,14 +1,19 @@
 process.env.NODE_ENV = 'development'
 const webpack = require('webpack')
+const del = require('del')
 
 /**
  * 启动 dev server
  */
 const startDevServer = () => {
   console.log('----- startDevServer -----')
+  
+  // 删除 dist 目录
+  del(['./dist'])
 
-  const renderConfig = require('../scripts/webpack.render.config.js')
+  const renderConfig = require('../config/webpack.render.config.js')
   const compiler = webpack(renderConfig)
+  compiler.run()
 
 
   console.log('----- devServerEnd -----')
