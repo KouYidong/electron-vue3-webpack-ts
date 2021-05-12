501fb2c622f167cbd627c2b62d22eacc04d867a8
kouyidong <1223519172@qq.com>
2021-05-12 21:42:34 +0800

    增加 webpack-dev-server, 未完成

--- builder/scripts/dev.js
+++ builder/scripts/dev.js
@@ -1,19 +1,35 @@
 process.env.NODE_ENV = 'development'
 const webpack = require('webpack')
 const del = require('del')
+const WebpackDevServer = require('webpack-dev-server')
 
 /**
  * 启动 dev server
  */
 const startDevServer = () => {
   console.log('----- startDevServer -----')
-  
+
   // 删除 dist 目录
   del(['./dist'])
 
   const renderConfig = require('../config/webpack.render.config.js')
+  const options = {
+    contentBase: renderConfig.output.path,
+    hot: true, // 热更新
+    // open: true, // 自动打开浏览器
+    // hotOnly: true,
+  }
+
+  // WebpackDevServer.addDevServerEntrypoints(renderConfig, options);
   const compiler = webpack(renderConfig)
-  compiler.run()
+
+  new WebpackDevServer(compiler, options).listen(8080, (err) => {
+    if (err) {
+      console.error(`WebpackDevServer listen error =>`, err)
+      return process.exit()
+    }
+    console.log(`Listening 8080 success`)
+  })
 
 
   console.log('----- devServerEnd -----')
