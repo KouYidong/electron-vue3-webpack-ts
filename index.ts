021c94f765380bc6962bf664b458ca9cfac0cc93
kouyidong <1223519172@qq.com>
2021-05-26 18:51:04 +0800

    更改窗口 load 方式

--- index.ts
+++ index.ts
@@ -1,11 +1,3 @@
-// import _ from 'lodash'
-
-// function test () {
-//   const element = document.createElement('div')
-//   element.innerHTML = _.join(['Hello', 'webpack'], '-')
-//   return element
-// }
-
 import { createApp } from 'Vue'
 import App from './App.vue'
 
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+const path = require('path')
+const url = require('url')
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
@@ -11,7 +13,11 @@ function createWindow () {
   })
 
   // 这里 load 的地址基于 webpack.main.config.js 中 output 中的 path 和 filename
-  win.loadFile('../index.html')
+  // pathToFileURL: 返回文件URL对象。
+  // const filePath:string = url.pathToFileURL(path.join(process.cwd(), './dist/index.html')).href
+  // 如果路径或者参数中含有中文，需要对路径进行编码处理
+  // win.loadURL(encodeURI(filePath))
+  win.loadURL('http://localhost:8080/')
 }
 
 app.whenReady().then(createWindow)
